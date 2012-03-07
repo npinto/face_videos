772330619246b811de4a1cd1ce7db9be1dc47178
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-06 23:05:41 -0500

    Various fixes

--- x_run_all.sh
+++ x_run_all.sh
@@ -3,6 +3,7 @@
 PWD=$(dirname $(realpath $0))
 
 test -f "$1"
+BNAME=$(basename "$1")
 
 ${PWD}/x_1_extract_frames_from_video.sh "$1"
-${PWD}/x_2_rotate_all_jpg_90.sh "$1"
+${PWD}/x_2_rotate_all_jpg_90.sh "${BNAME%.*}"
