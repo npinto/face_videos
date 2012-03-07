e910eabf6f6918fbcca9d107f4218ae5b3987066
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-06 21:50:09 -0500

    MISC

--- x_1_extract_frames_from_video.sh
+++ x_1_extract_frames_from_video.sh
@@ -8,9 +8,10 @@ command -v mplayer
 test -f "$1"
 
 VID_FNAME="$(realpath "$1")"
+VID_BNAME="$(basename "${VID_FNAME}")"
 echo $VID_FNAME
-DNAME=${VID_FNAME%.*}
+DNAME="${VID_BNAME%.*}"
 
 mkdir -p "${DNAME}"
 cd "${DNAME}"
-mplayer -vo jpeg "${VID_FNAME}"
+mplayer -cache 8192 -vo jpeg "${VID_FNAME}"
